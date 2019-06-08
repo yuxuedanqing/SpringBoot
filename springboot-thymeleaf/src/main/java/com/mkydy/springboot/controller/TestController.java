0a971a89a3de49173a3252dc2add95098c43f116
hll <202292735@qq.com>
2019-06-08 23:09:39 +0800

    添加thymeleaf模板

--- springboot-thymeleaf/src/main/java/com/mkydy/springboot/controller/TestController.java
+++ springboot-thymeleaf/src/main/java/com/mkydy/springboot/controller/TestController.java
@@ -2,10 +2,12 @@ package com.mkydy.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
+	@GetMapping("/test")
 	public String testThymeleaf(ModelMap modelMap) {
 		modelMap.addAttribute("msg", "Hello mkydy, this is thymeleaf");
 		return "thymeleaf";
