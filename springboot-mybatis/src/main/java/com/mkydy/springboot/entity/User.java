67007969a3d590b7582470f3b4ed741bb9e2d698
hll <202292735@qq.com>
2019-06-09 14:17:39 +0800

    整合mybatis

---
+++ springboot-mybatis/src/main/java/com/mkydy/springboot/entity/User.java
@@ -0,0 +1,50 @@
+package com.mkydy.springboot.entity;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("user")
+public class User {
+
+	private Integer id;
+	
+	private String user_name;
+	
+	private String user_password;
+
+	public User(String user_name, String user_password) {
+        this.user_name = user_name;
+        this.user_password = user_password;
+    }
+
+    public User(int id, String user_name, String user_password) {
+        this.id = id;
+        this.user_name = user_name;
+        this.user_password = user_password;
+    }
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	
+	
+}
