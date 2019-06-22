fb656b92884ba58c03731d2b52083ae97af3c0fb
hll <202292735@qq.com>
2019-06-22 21:27:57 +0800

    RBAC权限管理系统

---
+++ springboot-rbac/src/main/java/com/mkydy/springboot/SpringbootRbacApplication.java
@@ -0,0 +1,13 @@
+package com.mkydy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRbacApplication.class, args);
+	}
+
+}
