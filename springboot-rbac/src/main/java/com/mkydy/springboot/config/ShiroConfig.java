b23fa57ab56f9eed248d018f6be725dc1584f974
hll <202292735@qq.com>
2019-06-23 09:58:40 +0800

    使用shiro内置的拦截器

--- springboot-rbac/src/main/java/com/mkydy/springboot/config/ShiroConfig.java
+++ springboot-rbac/src/main/java/com/mkydy/springboot/config/ShiroConfig.java
@@ -1,5 +1,8 @@
 package com.mkydy.springboot.config;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,10 +23,42 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ShiroConfig {
 	
+	/**
+	 * 
+	 * @Description： 创建ShiroFilterFactoryBean
+	 * @author: Hll 
+	 * @since: 2019年6月22日 下午10:31:16
+	 */
+	@Bean
 	public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager")DefaultWebSecurityManager securityManager) {
 		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
 		// 设置安全管理器
 		shiroFilterFactoryBean.setSecurityManager(securityManager);
+		/**
+		 * 添加shiro内置过滤器，可以实现权限相关的拦截器
+		 * 常用过滤器：
+		 * 	anon：无需认证（登录）可以访问
+		 * 	authc：必须认证才可以访问
+		 * 	user：如果使用rememberMe的功能可以直接访问
+		 * 	perms：该资源必须的到资源权限才可以访问
+		 * 	role：该资源必须得到角色权限才可以访问
+		 */
+		Map<String,String> linkedHashMap = new LinkedHashMap<>();
+//		linkedHashMap.put("/addUser","authc");
+//		linkedHashMap.put("/updateUser","authc");
+		/**
+		 * 放行该请求
+		 */
+		linkedHashMap.put("/testThymeleaf", "anon");
+		/**
+		 * 使请求某路径下的所有资源都用统一过滤
+		 */
+		linkedHashMap.put("/*","authc");
+		/**
+		 * 修改拦截后跳转的页面
+		 */
+		shiroFilterFactoryBean.setLoginUrl("/toLogin");
+		shiroFilterFactoryBean.setFilterChainDefinitionMap(linkedHashMap);
 		return shiroFilterFactoryBean;
 	}
 
@@ -47,9 +82,12 @@ public class ShiroConfig {
 	 * @author: Hll 
 	 * @since: 2019年6月22日 下午10:16:22
 	 */
-	@Bean
+	@Bean(name="shiroRealm")
 	public ShiroRealm getRealm() {
 		return new ShiroRealm();
 	}
+	
+	
+	
 	
 }
--- springboot-rbac/src/main/java/com/mkydy/springboot/controller/HelloController.java
+++ springboot-rbac/src/main/java/com/mkydy/springboot/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.mkydy.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 /**
@@ -31,4 +32,32 @@ public class HelloController {
 		model.addAttribute("name", "硝酸银");
 		return "test";
 	}
+	
+	/**
+	 * 
+	 * @Description： 用户添加
+	 * @author: Hll 
+	 * @since: 2019年6月23日 上午9:24:58
+	 */
+	@GetMapping(value= "/addUser")
+	public String addUser() {
+		return "user/addUser";
+	}
+	
+	/**
+	 * 
+	 * @Description： 用户修改
+	 * @author: Hll 
+	 * @since: 2019年6月23日 上午9:25:15
+	 */
+	@GetMapping(value= "/updateUser")
+	public String updateUser() {
+		return "user/updateUser";
+	}
+	
+	@GetMapping(value="/toLogin")
+	public String login() {
+		return "login";
+	}
+	
 }
