9a94f8f4d49be0424b60bacfc230523daf41c196
snails <202292735@qq.com>
2020-05-06 21:09:21 +0800

    删除所有项目

--- springboot-jpa/src/main/java/com/mkydy/springboot/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mkydy.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mkydy.springboot.entity.City;
-
-public interface CityRepository extends JpaRepository<City, Integer> {
-
-}
